f1bc9a10639764962135ba8eae8c679332a2cea1
Sheng <cyusheng93@gmail.com>
2023-06-25 11:19:06 +0100

    Added posts controller and layout page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,14 @@
 class HomeController < ApplicationController
   def index
+    set_meta_tags
+      title: "My Page Title",
+      description: "This is a description of my page.",
+      keywords: "keyword1, keyword2, keyword3",
+      og: {
+        title: "My Page Title",
+        description: "This is a description of my page.",
+        image: "url_to_image"
+      },
+      canonical: "https://www.example.com/my-page-url"
   end
 end
